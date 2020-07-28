11f2f5cd73d72a5640ef11851ef66c0ad34c52b6
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-07-27 19:01:12 -0500

    elementbyID, event listeners

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("testing")
+const firstFunc = (arg) => {
+    return `This is a ${arg}`
+}
+
+
+
+const secondFunc = (arg2) => {
+    return `This is another ${arg2}`
+}
+
+const thirdFunc = (string3) => {
+    return "This is a sentence"
+}
+
+
+const mainButton = document.getElementById('main-btn');
+
+// console.log(mainButton)
+
+const button = document.querySelector(`#main-btn`)
+
+// console.log(button)
+
+const youClickedMe = () => {
+    console.log("You clicked me")
+}
+
+button.addEventListener ("click", youClickedMe)
